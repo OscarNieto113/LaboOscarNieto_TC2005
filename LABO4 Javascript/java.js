2fd2107d74f4a9ca75dc75e82a08ef76c10a2e8f
Oscar Nieto <a01705090@tec.mx>
2022-02-19 20:05:04 -0600

    Laboratorio 4 terminado

--- LABO4 Javascript/java.js
+++ LABO4 Javascript/java.js
@@ -21,4 +21,121 @@ function tabla_cuadrados(){
   table += "</table>";
   return table;
 }
+
+//Promebla 2
+function suma (){
+  const num1 = Math.floor(Math.random() * (100 - 1)) + 1;// Retorna un número aleatorio entre min (incluido) y max (excluido)
+  const num2 = Math.floor(Math.random() * (10 - 1)) + 1;
+
+  const inicio = Date.now();//Inicia Cronometro
+  const respuesta = prompt(`Ingrese el resultado de la operacion: ${num1} + ${num2}`);
+  const final = Date.now();//Termina Cronometro
+
+  const duracion = (final - inicio) / 1000;
+
+  let mensaje = `El resultado es ${respuesta == num1+num2 ? 'correcto' : 'incorrecto'}. Tardaste ${Math.round(duracion)} segundos`;
+  return mensaje;
+}
+
+//Promebla 3
+function contador (lista){
+  let contPositivo =0;
+  let contNegativo =0;
+  let contCero =0;
+  let lenght = lista.lenght;
+  for (i = 0; i <= 40; i++){
+    if (lista[i] == 0 ){
+      contCero++;
+    }
+    else if (lista[i] > 0) {
+      contPositivo++;
+    }
+    else{
+      contNegativo++;
+    }
+  }
+
+  let mensaje = "Positivos: " +contPositivo+ " Negativos: " +contNegativo+ " Ceros: " +contCero+ ". <br>Lista: <br> [" +lista+ "].";
+  return mensaje;
+}
+
+function randomlist(){
+  const randomlist = [0];
+  for (i = 1; i <= 40; i++){
+    var num = Math.floor(Math.random()*99) + 1; // this will get a number between 1 and 99;
+    num *= Math.round(Math.random()) ? 1 : -1; // this will add minus sign in 50% of cases
+    randomlist.push(num);
+  }
+  return randomlist
+}
+
+//Problema 4
+function promedio (){
+  let matrix = [[1,2,3,4,5,6],[9,8,7,6,5,4],[1,2,3,4,5,6],[9,8,7,6,5,4]];
+  const prom = (matriz) => matriz.map((arr, i, matrix) => arr.reduce((curr, num) => curr + num,0) / matrix.length);
+  let mensaje = `Matrix:<br>[1,2,3,4,5,6]<br>[9,8,7,6,5,4]<br>[1,2,3,4,5,6]<br>[9,8,7,6,5,4]<br>Promedios: <br>` + prom(matrix).join(', ') + '';
+  return mensaje;
+}
+
+//Problema 5
+
+function inverso (){
+  const num1 = prompt('Ingresa un numero de dos o mas digitos');
+  const inverso = (num) => num.toString().split('').reverse().join('');
+  let mensaje = "Numero que escribiste: " +num1+ " Numero Inverso: " +inverso(num1)+ ". ";
+  return mensaje;
+}
+
+//Problema 6
+// Definicion de la clase. Arbol binario
+class Node{
+  constructor(item){
+    this.data = item;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+var root = null;
+
+//Solucion:
+function lca(root, n1, n2){
+  while (root != null){
+
+    // Si ambos numeros son menores a la raiz entonces el lca esta a la iquierda
+    if (root.data > n1 && root.data > n2)
+        root = root.left;
+    // Si ambos numeros son mayores a la raiz entonces el lca esta a la derecha
+    else if (root.data < n1 && root.data < n2)
+        root = root.right;
+
+    else break;
+  }
+  return root;
+}
+
+function lowestCommonAncestor (){
+  //Creamos el objeto
+  root = new Node(20);
+  root.left = new Node(8);
+  root.right = new Node(22);
+  root.left.left = new Node(4);
+  root.left.right = new Node(12);
+  root.left.right.left = new Node(10);
+  root.left.right.right = new Node(14);
+
+  var n1 = 10, n2 = 14;
+  var t = lca(root, n1, n2);
+
+  let mensaje = "El ancestro comun entre " + n1 + " y " + n2 + " es: " + t.data + ".";
+  return mensaje;
+}
+
+
+
 document.getElementById("resultado1").innerHTML = tabla_cuadrados(); //Se manda a llamar la funcion
+document.getElementById("resultado2").innerHTML = suma(); //Se manda a llamar la funcion
+document.getElementById("resultado3").innerHTML = contador(randomlist()); //Se manda a llamar la funcion
+document.getElementById("resultado4").innerHTML = promedio(); //Se manda a llamar la funcion
+document.getElementById("resultado5").innerHTML = inverso(); //Se manda a llamar la funcion
+document.getElementById("resultado6").innerHTML = lowestCommonAncestor(); //Se manda a llamar la funcion
