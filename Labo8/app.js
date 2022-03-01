78c83b2031ffb4f62b6a649789c61f5708bb92ae
Oscar Nieto <a01705090@tec.mx>
2022-02-28 20:57:50 -0600

    Labo 8 completo

--- Labo8/app.js
+++ Labo8/app.js
@@ -1,36 +1,66 @@
+//cd "Labo8"
 //node ./app.js   ejecutar
-//http://localhost:3000/
+//http://localhost:3000/   Pagina
 
 console.log("hola desde node");
 
 //fs: filesystem
-const filesystem = require('fs');
-
-filesystem.writeFileSync('hola.txt', 'Hola desde node');
-
-
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-
-for (let item of arreglo) {
-    setTimeout(() => {
-        console.log(item);
-    }, item);
+//Problema 1
+function promedio(arreglo){
+    let suma = 0;
+    for(let i of arreglo){
+        suma += i;
+    }
+    return suma/(arreglo.length);
 }
 
+//Problema 2
+const text = (cadena) => {
+    const filesystem = require('fs')
+    filesystem.writeFileSync('hola.txt', cadena)
+}
 
+//Problema 3
+//Genera un array de números aleatorios, ordenalos y muéstralos en una lista desordenada.
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-const http = require('http');
+function ordenar() {
+    let array = [];
+    //Se rellena el arreglo con numeros aleatorios
+    for (let i = 0; i < 10; i++) {
+        array.push(getRandomInt(1, 100));
+    }
+    console.log("Array sin ordenar");
+    console.log(array);
+    array.sort(function (a, b) { return a - b });
+    console.log("Array ordenado");
+    console.log(array);
+}
 
-const server = http.createServer( (request, response) => {
+const arreglo = [16, 95, 34, 84, 29, 74, 18, 46, 54, 41];
 
-    console.log(request.url);
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<!DOCTYPE html>');
-    response.write('<html><head>');
-    response.write('<title>Cerveza</title>');
-    response.write('</head><body>');
-    response.write('<h1 id="principal">La historia de la cerveza</h1>');
-    response.end();
-});
+promedio(arreglo);
+text("Hola mundo :)");
+ordenar();
 
-server.listen(3000);
+// Petición al servidor que devuelve laboratorio 1
+const http = require('http')
+const fs = require('fs').promises;
+
+const server = http.createServer((req, res) => {
+    fs.readFile("../LABO1 HTML/LABO1.html")
+    .then(contents => {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.end(contents);
+    })
+    .catch(err => {
+        res.writeHead(500);
+        res.end(err);
+        return;
+    });
+})
+
+server.listen(3000)
