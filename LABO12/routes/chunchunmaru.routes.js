51fb73940e83cdc0c6f49be8adbe43929caecab4
OscarNieto <a01705090@itesm.mx>
2022-03-08 08:15:30 -0600

    Laboratorio 12 Completo

--- LABO12/routes/chunchunmaru.routes.js
+++ LABO12/routes/chunchunmaru.routes.js
@@ -36,15 +36,58 @@ router.get('/cerveza', (request, response, next) => {
 //------------------------
 router.get('/mistborn', (request, response, next) => {
     console.log('GET /chunchunmaru/mistborn');
-    response.render('mistborn', {nombre: 'Lalo'});
+    response.render('mistborn', {nombre: 'Chunchunmaru'});
 });
 
 router.post('/mistborn', (request, response, next) => {
     console.log('POST /chunchunmaru/mistborn');
     console.log(request.body);
-    capybaras.push({nombre: request.body.nombre});
+    mistborn.push({nombre: request.body.nombre});
     response.redirect('/chunchunmaru');
 });
+
+//---------------------------
+
+router.get('/standalones', (request, response, next) => {
+    console.log('GET /chunchunmaru/standalones');
+    response.render('standalones', {nombre: 'Chunchunmaru'});
+});
+
+router.post('/standalones', (request, response, next) => {
+    console.log('POST /chunchunmaru/standalones');
+    console.log(request.body);
+    standalones.push({nombre: request.body.nombre});
+    response.redirect('/chunchunmaru');
+});
+
+//---------------------------
+
+router.get('/storm', (request, response, next) => {
+    console.log('GET /chunchunmaru/storm');
+    response.render('storm', {nombre: 'Chunchunmaru'});
+});
+
+router.post('/storm', (request, response, next) => {
+    console.log('POST /chunchunmaru/storm');
+    console.log(request.body);
+    stormlight.push({nombre: request.body.nombre});
+    response.redirect('/chunchunmaru');
+});
+
+//---------------------------
+
+router.get('/whitesand', (request, response, next) => {
+    console.log('GET /chunchunmaru/whitesand');
+    response.render('whitesand', {nombre: 'Chunchunmaru'});
+});
+
+router.post('/whitesand', (request, response, next) => {
+    console.log('POST /chunchunmaru/whitesand');
+    console.log(request.body);
+    whitesand.push({nombre: request.body.nombre});
+    response.redirect('/chunchunmaru');
+});
+
 //---------------------------
 
 router.use('/', (request, response, next) => {
