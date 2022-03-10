2c2b619b58e1ca99e33d6bc391c32399faf644b5
Oscar Nieto <a01705090@tec.mx>
2022-03-09 20:48:06 -0600

    Funciona c:

--- LABO14/app.js
+++ LABO14/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const rutas_chunchunmaru = require('./routes/chunchunmaru.routes');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const rutas_users = require('./routes/user.routes');
 
 //Para devolver como respuesta un archivo html desde express, se requiere el módulo path, puedes incorporarlo con const path = require('path'); al inicio de tus archivos de código.
 const path = require('path');
@@ -13,9 +16,19 @@ app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'tesdrcfvgybhunjimkoiouhygturdyesrydctvybunjiouyt68d43a577yb7vc6543s475r6tvy',
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
 
 //ruta url
 app.use('/chunchunmaru', rutas_chunchunmaru);
+app.use('/users', rutas_users);
+
+//app.use(bodyParser.urlencoded({extended: false}));
+
 
 //Middleware
 app.use((request, response, next) => {
--- LABO14/controllers/chunchunmaru_controller.js
+++ LABO14/controllers/chunchunmaru_controller.js
@@ -6,64 +6,115 @@ const Whitesand = require('../models/whitesand');
 
 exports.get_mistborn = (request, response, next) => {
     console.log('GET /chunchunmaru/mistborn');
-    response.render('mistborn', {nombre: 'Chunchunmaru'});
+    response.render('mistborn', {
+      //
+        username: request.session.username ? request.session.username : '',
+        info: ''
+    });
 };
+//
 
 exports.post_mistborn = (request, response, next) => {
     console.log('POST /chunchunmaru/mistborn');
     console.log(request.body);
     const mistborn = new Mistborn(request.body.nombre);
     mistborn.save();
+    //
+    request.session.info = mistborn.nombre + ' fue agregado con éxito';
+    response.setHeader('Set-Cookie', 'ultimo_mistborn='+mistborn.nombre+'; HttpOnly');
     response.redirect('/chunchunmaru');
 };
+//
 //-------------------------------------------
 
 exports.get_standalones = (request, response, next) => {
     console.log('GET /chunchunmaru/standalones');
-    response.render('standalones', {nombre: 'Chunchunmaru'});
+    response.render('standalones', {
+    //
+    username: request.session.username ? request.session.username : '',
+    info: ''
+  });
 };
+//
 
 exports.post_standalones = (request, response, next) => {
     console.log('POST /chunchunmaru/standalones');
     console.log(request.body);
     const standalones = new Standalones(request.body.nombre);
     standalones.save();
+    //
+    request.session.info = standalones.nombre + ' fue agregado con éxito';
+    response.setHeader('Set-Cookie', 'ultimo_standalones='+standalones.nombre+'; HttpOnly');
     response.redirect('/chunchunmaru');
 };
+//
 
 //---------------------------------------------
 
 exports.get_storm = (request, response, next) => {
     console.log('GET /chunchunmaru/storm');
-    response.render('storm', {nombre: 'Chunchunmaru'});
+    response.render('storm', {
+    //
+      username: request.session.username ? request.session.username : '',
+      info: ''
+  });
 };
+//
 
 exports.post_storm = (request, response, next) => {
     console.log('POST /chunchunmaru/storm');
     console.log(request.body);
     const storm = new Storm(request.body.nombre);
     storm.save();
+    //
+    request.session.info = storm.nombre + ' fue agregado con éxito';
+    response.setHeader('Set-Cookie', 'ultimo_storm='+storm.nombre+'; HttpOnly');
     response.redirect('/chunchunmaru');
 };
+//
 
 //----------------------------------------------------
 
 exports.get_whitesand = (request, response, next) => {
     console.log('GET /chunchunmaru/whitesand');
-    response.render('whitesand', {nombre: 'Chunchunmaru'});
+    response.render('whitesand', {
+    //
+      username: request.session.username ? request.session.username : '',
+      info: ''
+  });
 };
+//
 
 exports.post_whitesand = (request, response, next) => {
     console.log('POST /chunchunmaru/whitesand');
     console.log(request.body);
     const whitesand = new Whitesand(request.body.nombre);
     whitesand.save();
+    //
+    request.session.info = whitesand.nombre + ' fue agregado con éxito';
+    response.setHeader('Set-Cookie', 'ultimo_whitesand='+whitesand.nombre+'; HttpOnly');
     response.redirect('/chunchunmaru');
 };
+//
 
 //---------------------------------------------
 
 exports.listar = (request, response, next) => {
     console.log('Ruta /chunchunmaru');
-    response.render('lista', {mistborn: Mistborn.fetchAll(), standalones: Standalones.fetchAll(), storm: Storm.fetchAll(), whitesand: Whitesand.fetchAll()});
+    //console.log(request.get('Cookie').split('=')[1]);
+    console.log(request.cookies);
+    const info = request.session.info ? request.session.info : '';
+    request.session.info = '';
+    response.render('lista', {
+      mistborn: Mistborn.fetchAll(),
+      standalones: Standalones.fetchAll(),
+      storm: Storm.fetchAll(),
+      whitesand: Whitesand.fetchAll(),
+      username: request.session.username ? request.session.username : '',
+      ultimo_mistborn: request.cookies.ultimo_mistborn ? request.cookies.ultimo_mistborn : '',
+      ultimo_standalones: request.cookies.ultimo_standalones ? request.cookies.ultimo_standalones : '',
+      ultimo_storm: request.cookies.ultimo_storm ? request.cookies.ultimo_storm : '',
+      ultimo_whitesand: request.cookies.ultimo_whitesand ? request.cookies.ultimo_whitesand : '',
+      info: info //El primer info es la variable del template, el segundo la constante creada arriba
+  });
 }
